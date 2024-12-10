cba807013b2f6eee305916863ac47dea177d043e
ChibiBibiBa <marcinstanisz43@interia.pl>
2024-12-10 09:29:40 +0100

    quicc fix

---
+++ CalculatorMultiplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public partial class Calculator
+    {
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
